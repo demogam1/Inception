67f299a188e408594906dbc0d03c9f7894fb6a09
jeanMichelCodeur42 <toto@gmail.com>
2022-06-23 17:23:38 +0200

    wp config modifier pour cause de maux de tete

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -37,6 +37,8 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'WP_HOME', 'http://misaev.42.fr' );
+define( 'WP_SITEURL', 'https://misaev.42.fr' );
 /**#@+
  * Authentication unique keys and salts.
  *
